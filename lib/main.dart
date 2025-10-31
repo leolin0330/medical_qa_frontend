144de086063d36873347e5f8df281f3cb5c0b56e
leolin0330 <leolin388@gmail.com>
2025-10-31 14:25:06 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,63 +9,71 @@ import 'pages/me_page.dart';
 // 把整個 build/web 資料夾丟到Netlify Flutter Web
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
-const MyApp({super.key});
+  const MyApp({super.key});
 
-
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Medical QA',
-debugShowCheckedModeBanner: false,
-theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
-home: const HomeShell(),
-);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Medical QA',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.teal),
+      home: const HomeShell(),
+    );
+  }
 }
-}
-
 
 class HomeShell extends StatefulWidget {
-const HomeShell({super.key});
+  const HomeShell({super.key});
 
-
-@override
-State<HomeShell> createState() => _HomeShellState();
+  @override
+  State<HomeShell> createState() => _HomeShellState();
 }
-
 
 class _HomeShellState extends State<HomeShell> {
-int _index = 0;
+  int _index = 0;
 
+  final _pages = const [
+    const QAPage(),
+    const EncyclopediaPage(),
+    const LearningPage(),
+    const NewsPage(),
+    const MePage(),
+  ];
 
-final _pages = const [
-QaPage(),
-EncyclopediaPage(),
-LearningPage(),
-NewsPage(),
-MePage(),
-];
-
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-body: SafeArea(child: _pages[_index]),
-bottomNavigationBar: NavigationBar(
-selectedIndex: _index,
-onDestinationSelected: (i) => setState(() => _index = i),
-destinations: const [
-NavigationDestination(icon: Icon(Icons.question_answer_outlined), selectedIcon: Icon(Icons.question_answer), label: '問答'),
-NavigationDestination(icon: Icon(Icons.menu_book_outlined), selectedIcon: Icon(Icons.menu_book), label: '百科'),
-NavigationDestination(icon: Icon(Icons.school_outlined), selectedIcon: Icon(Icons.school), label: '學習'),
-NavigationDestination(icon: Icon(Icons.flash_on_outlined), selectedIcon: Icon(Icons.flash_on), label: '快訊'),
-NavigationDestination(icon: Icon(Icons.person_outline), selectedIcon: Icon(Icons.person), label: '我的'),
-],
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: _pages[_index]),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (i) => setState(() => _index = i),
+        destinations: const [
+          NavigationDestination(
+              icon: Icon(Icons.question_answer_outlined),
+              selectedIcon: Icon(Icons.question_answer),
+              label: '問答'),
+          NavigationDestination(
+              icon: Icon(Icons.menu_book_outlined),
+              selectedIcon: Icon(Icons.menu_book),
+              label: '百科'),
+          NavigationDestination(
+              icon: Icon(Icons.school_outlined),
+              selectedIcon: Icon(Icons.school),
+              label: '學習'),
+          NavigationDestination(
+              icon: Icon(Icons.flash_on_outlined),
+              selectedIcon: Icon(Icons.flash_on),
+              label: '快訊'),
+          NavigationDestination(
+              icon: Icon(Icons.person_outline),
+              selectedIcon: Icon(Icons.person),
+              label: '我的'),
+        ],
+      ),
+    );
+  }
 }
